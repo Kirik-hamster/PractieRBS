51e99d225d1a0e7c7ab78185f8afb1fe0b6150dd
Kirik-hamster <mkir2@k.ru>
2024-03-27 16:30:57 +0400

    open and read file

--- hello.go
+++ hello.go
@@ -3,16 +3,34 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"log"
+	"os"
 	"time"
 )
 
 func main() {
 	start := time.Now()
-	nameTo := flag.String("name", "world", "name to sell hi")
+
+	src := flag.String("src", "", "Source file path")
 	flag.Parse()
 
-	fmt.Printf("Hello, %s!\n", *nameTo)
+	if *src == "" {
+		log.Fatal("Source file path is not specified. Use --src flag to specify the file path.")
+	}
+
+	file, err := os.Open(*src)
+	if err != nil {
+		log.Fatalf("Error opening file: %v", err)
+	}
+
+	content, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatalf("Error reading file %v", err)
+	}
+
+	fmt.Printf("Error reaing file:\n\n%s\n", content)
 
 	elapsed := time.Since(start)
-	fmt.Printf("Program execution time: %s\n", elapsed)
+	fmt.Printf("\nProgram execution time: %s\n", elapsed)
 }
