d3433d88cd4920677f50230b1281f25bc83e5b12
Kirik-hamster <mkir2@k.ru>
2024-03-28 17:18:11 +0400

    Refactor: Split program into functions

--- hello.go
+++ hello.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"io"
@@ -9,7 +10,6 @@ import (
 	"net/url"
 	"os"
 	"path/filepath"
-	"bufio"
 	"strings"
 	"time"
 )
@@ -22,7 +22,7 @@ func main() {
 	flag.Parse()
 
 	if *src == "" || *dst == "" {
-		log.Fatal("Source file path is not specified. Use --src flag to specify the file path.")
+		log.Fatal("Source file path and destination directory path must be specified. Use --src and --dst flags to specify them.")
 	}
 
 	srcFile, err := os.Open(*src)
@@ -38,47 +38,63 @@ func main() {
 		if url == "" {
 			continue
 		}
+		fetchAndSave(url, *dst)
 
-		resp, err := http.Get(url)
-		if err != nil {
-			log.Printf("Error fetching URL: %v\n", err)
-			continue
-		}
-		defer resp.Body.Close()
-
-		if resp.StatusCode != 200 {
-			log.Printf("Unexpected status code for URL %s: %d\n", url, resp.StatusCode)
-			continue
-		}
-
-		fileName, err := getFileNameFromURL(url) 
-		if fileName == "" {
-			continue
-		}
-		if err != nil {
-			log.Printf("err: %v\n", err)
-		}
-		fileName += ".html"
-		filePath := filepath.Join(*dst, fileName)
-
-		dstFile, err := os.Create(filePath)
-		if err != nil {
-			log.Fatalf("Error creating destination file: %v\n", err)
-		}
-		_, err = io.Copy(dstFile, resp.Body)
-		defer dstFile.Close()
-		
-		if err != nil {
-			log.Fatalf("Error copying content: %v\n", err)
-		}
-		fmt.Printf("File copied successfully to \n%s\n", filePath)
 	}
-
 
 	elapsed := time.Since(start)
 	fmt.Printf("\nProgram execution time: %s\n", elapsed)
 }
 
+// fetchAndSave() функця считывает urlStr и пытается отправть get запрос по этому url,
+// затем, если успешный запрос, сохраняет полученый body с запроса и сохраняет по
+// пути dst
+func fetchAndSave(url, dst string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Printf("Error fetching URL: %v\n", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		log.Printf("Unexpected status code for URL %s: %d\n", url, resp.StatusCode)
+		return
+	}
+
+	fileName, err := getFileNameFromURL(url)
+	if fileName == "" {
+		return
+	}
+	if err != nil {
+		log.Printf("err: %v\n", err)
+	}
+	fileName = fmt.Sprintf("%s.html", fileName)
+
+	if dst == "./" {
+		dst = "./list"
+		err := os.MkdirAll(dst, 0755)
+		if err != nil {
+			log.Fatalf("Error creating folder %s: %v\n", dst, err)
+		}
+	}
+	filePath := filepath.Join(dst, fileName)
+
+	dstFile, err := os.Create(filePath)
+	if err != nil {
+		log.Fatalf("Error creating destination file: %v\n", err)
+	}
+	_, err = io.Copy(dstFile, resp.Body)
+	defer dstFile.Close()
+
+	if err != nil {
+		log.Fatalf("Error copying content: %v\n", err)
+	}
+	fmt.Printf("File copied successfully to \n%s\n", filePath)
+}
+
+// getFileNameFromURL() функция получает url сайт
+// и возвращает имя файла на основе доменного имени url
 func getFileNameFromURL(siteURL string) (string, error) {
 	parsedURL, err := url.Parse(siteURL)
 	if err != nil {
@@ -95,7 +111,6 @@ func getFileNameFromURL(siteURL string) (string, error) {
 	if len(parts) < 2 {
 		return domain, nil
 	}
-
 
 	return parts[0], nil
 }
